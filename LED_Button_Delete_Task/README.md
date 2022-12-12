0eda5bdc3b43aa63f0d6c587a2e76c44b69ea0a2
abdallah rabie <76526170+Rabie45@users.noreply.github.com>
2022-12-12 22:04:29 +0200

    Delete Task tutorial

---
+++ LED_Button_Delete_Task/README.md
@@ -0,0 +1,38 @@
+# FreeRTOS Template
+
+## In this part New Functions APIs we gonna use
+
+BaseType_t xTaskNotify( TaskHandle_t xTaskToNotify,
+                         uint32_t ulValue,
+                         eNotifyAction eAction );
+                         
+                         
+- TaskNotify() is used to send an event directly to and potentially unblock an RTOS task
+
+ **Parameter**\
+TaskHandle_t xTaskToNotify : The handle of the RTOS task being notified. This is the target task.
+uint32_t ulValue : Used to update the notification value of the target task. 
+eNotifyAction eAction : An enumerated type that can take one of the values documented in the table below in order to perform the associated action.
+
+eAction have multible possible case :
+- eNoAction = 0  Notify the task without updating its notify value
+- eSetBits,	Set bits in the task's notification value. 
+- eIncrement  Increment the task's notification value. 
+- eSetValueWithOverwrite  Set the task's notification value to a specific value even if the previous value has not yet been read by the task. 
+- eSetValueWithoutOverwrite Set the task's notification value if the previous value has been read by the task. 
+
+xTaskNotifyWait( uint32_t ulBitsToClearOnEntry,
+                             uint32_t ulBitsToClearOnExit,
+                             uint32_t *pulNotificationValue,
+                             TickType_t xTicksToWait );
+                             
+- this function used to wait until an event happen
+
+ **Parameter**
+ - uint32_t ulBitsToClearOnEntry : if ulBitsToClearOnEntry is 0x01, then bit 0 of the task's notification value will be cleared on entry to the function.
+ - uint32_t ulBitsToClearOnExit : if ulBitsToClearOnExit is 0x03, then bit 0 and bit 1 of the task's notification value will be cleared before the function exits.
+ - uint32_t *pulNotificationValue : Used to pass out the RTOS task's notification value. 
+ - TickType_t xTicksToWait : The maximum time to wait in the Blocked state for a notification to be received if a notification is not already pending when xTaskNotifyWait() is called.
+
+__flowcontrol__
+- when the button is pressed the buttontask is goning to notfiy the led task to work without pressing the ledtask will be blocked 
---
+++ LED_Button_Delete_Task/src/main.c
@@ -0,0 +1,171 @@
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  Ac6
+ * @version V1.0
+ * @date    01-December-2013
+ * @brief   Default main function.
+ ******************************************************************************
+ */
+
+#include "stm32f3xx.h"
+#include "stm32f3xx_nucleo.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "string.h"
+#include "SEGGER_SYSVIEW.h"
+//extern void initialise_monitor_handles();
+
+//some macros
+#define TRUE 1
+#define FALSE 0
+#define AVAILABLE TRUE
+#define NOT_AVAILABLE FALSE
+
+/*******************Variables**************************/
+
+
+uint16_t buttomValue=GPIO_PIN_RESET;
+TaskHandle_t xTaskHandler1 = NULL;
+TaskHandle_t xTaskHandler2 = NULL;
+TaskFunction_t pxTaskCode;
+UART_HandleTypeDef uart;
+char user_data[250];
+uint16_t UART_ACCESS_KEY = AVAILABLE;
+
+/*******************Functions**************************/
+void USART_SendData(USART_TypeDef* USARTx, uint16_t Data);
+uint8_t USART_GetFlagStatus(USART_TypeDef* USARTx, uint32_t USART_FLAG);
+void USART2_init(void);
+void GPIO_Init();
+void Error_handler();
+void prm(char *msg);
+void rtos_delay(uint32_t delay_in_ms);
+void ButtonTask(void *params);
+void LEDTask(void *params);
+
+
+
+int main(void) {
+
+	DWT->CTRL |=(1<<0);
+	//initialise_monitor_handles();
+	HAL_Init();
+	GPIO_Init();
+	USART2_init();
+
+	//sprintf(user_data,"the app is running \r\n");
+	//prm(user_data);
+
+	/*Start SEEGER Captures*/
+	SEGGER_SYSVIEW_Conf();
+	SEGGER_SYSVIEW_Start();
+
+	/*Create two Taskes*/
+	xTaskCreate(LEDTask, "LED Task", 500, NULL, 2,
+			&xTaskHandler1);
+		xTaskCreate(ButtonTask, "Button Task", 500, NULL, 2,
+				&xTaskHandler2);
+
+
+	vTaskStartScheduler();
+	for (;;);
+}
+
+void LEDTask(void *params) {
+	while (1) {
+
+//wait an event going to block state untilkl it recives an interrupt
+	vTaskDelay(200);
+	HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_5);
+
+
+	}
+}
+void ButtonTask(void *params) {
+	while (1) {
+		/*Check the state of the button*/
+
+		if(HAL_GPIO_ReadPin(GPIOC,GPIO_PIN_13) == GPIO_PIN_SET)
+		{
+			rtos_delay(100);
+			//Notify to the Handler of the led task
+			HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_5);
+		}
+		else{
+			vTaskDelete(xTaskHandler1);
+		}
+	}
+}
+void USART2_init(void) {
+
+	uart.Instance = USART2;
+	uart.Init.BaudRate = 115200;
+	uart.Init.Mode = USART_MODE_TX_RX;
+	uart.Init.WordLength = UART_WORDLENGTH_8B;
+	uart.Init.StopBits = USART_STOPBITS_1;
+	uart.Init.Parity = USART_PARITY_NONE;
+	uart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	uart.Init.OverSampling = UART_OVERSAMPLING_16;
+	while (HAL_UART_Init(&uart) != HAL_OK) {
+		Error_handler();
+	}
+
+}
+void GPIO_Init(){
+
+	GPIO_InitTypeDef led_gpio,button_gpio;
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	led_gpio.Mode=GPIO_MODE_OUTPUT_PP;
+	led_gpio.Pin=GPIO_PIN_5;
+	led_gpio.Speed=GPIO_SPEED_FREQ_MEDIUM;
+	led_gpio.Pull=GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA,&led_gpio);
+
+	button_gpio.Mode=GPIO_MODE_INPUT;
+	button_gpio.Pin=GPIO_PIN_13;
+	button_gpio.Speed=GPIO_SPEED_FREQ_MEDIUM;
+	button_gpio.Pull=GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOC,&button_gpio);
+
+}
+void Error_handler() {
+
+	while (1)
+		;
+}
+void prm(char *msg) {
+	uint16_t len = strlen(msg);
+
+for(uint32_t i=0;i<len;i++){
+	while(USART_GetFlagStatus(USART2,USART_FLAG_TXE)!= SET);
+	USART_SendData(USART2,msg[i]);
+}
+}
+void USART_SendData(USART_TypeDef* USARTx, uint16_t Data)
+{
+  /* Transmit Data */
+  USARTx->TDR = (Data & (uint16_t)0x01FF);
+}
+uint8_t USART_GetFlagStatus(USART_TypeDef* USARTx, uint32_t USART_FLAG)
+{
+  uint8_t bitstatus = RESET;
+
+  if ((USARTx->ISR & USART_FLAG) != (uint16_t)RESET)
+  {
+    bitstatus = SET;
+  }
+  else
+  {
+    bitstatus = RESET;
+  }
+  return bitstatus;
+}
+
+void rtos_delay(uint32_t delay_in_ms){
+
+	uint32_t current_ticks= xTaskGetTickCount();
+	uint32_t delay_in_ticks=(delay_in_ms * configTICK_RATE_HZ) /1000;
+	while(xTaskGetTickCount() <(current_ticks+delay_in_ticks));
+}
